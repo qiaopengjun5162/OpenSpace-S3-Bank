f7ce51208745d7a85b80ae35d212f7aad95cd1ef
QiaoPengjun5162 <qiaopengjun0@gmail.com>
2024-07-04 18:11:55 +0800

    feat: configure Sepolia RPC and Etherscan for BankScript

--- script/Bank.s.sol
+++ script/Bank.s.sol
@@ -2,11 +2,16 @@
 pragma solidity ^0.8.20;
 
 import {Script, console} from "forge-std/Script.sol";
+import "../src/Bank.sol";
 
 contract BankScript is Script {
     function setUp() public {}
 
     function run() public {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        vm.stopBroadcast();
         vm.broadcast();
     }
 }
